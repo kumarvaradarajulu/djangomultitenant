ced2b210eaa6704399574c2e0d71c3c8c1355968
Akhil Lawrence <akhilputhiry@gmail.com>
2017-04-20 00:30:23 +0530

    bumped up version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name="djangomultitenant",
-    version="0.0.1",
+    version="0.0.2",
     author="Akhil Lawrence",
     author_email="akhilputhiry@gmail.com",
     description=("A django app which helps to implement multitenancy easily"),
@@ -20,9 +20,12 @@ setup(
     install_requires=[str(i.req) for i in parse_requirements("./requirements.txt", session=PipSession())],
     long_description=read("README.md"),
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 4 - Beta",
         "Topic :: Internet :: WWW/HTTP",
+        "Framework :: Django :: 1.10",
+        "Framework :: Django :: 1.11",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
         "License :: Freeware",
     ],
 )
